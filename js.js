6b8106d3defbc7467e1e5b3fef8a3d5fade1abfc
ArturoGSolis <47283548+ArturoGSolis@users.noreply.github.com>
2022-08-03 13:26:48 -0500

    barra de navegacion fija

---
+++ js.js
@@ -0,0 +1,29 @@
+
+window.onscroll = function () {
+    myFunction()
+};
+
+var navbar = document.getElementById("nav");
+var sticky = navbar.offsetTop;
+
+const btn_scrolltop = document.getElementById("btn_scrolltop")
+btn_scrolltop.addEventListener('click', () => {
+    window.scrollTo(0, 0)
+})
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+
+    if (window.scrollY < 600) {
+        btn_scrolltop.classList.remove("btn-scrolltop-on")
+    } else {
+        btn_scrolltop.classList.add("btn-scrolltop-on")
+    }
+}
+
+
+
